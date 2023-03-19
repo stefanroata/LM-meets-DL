2e01c0a348c30a11a2cd4d5efb7f72c8310bc957
Stefan-Cristian Roata <stefan.roata@u.yale-nus.edu.sg>
2023-03-20 01:04:47 +0800

    Add static demo page; fix wide mode issue

--- Introduction.py
+++ Introduction.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Introduction",
     page_icon="👋",
+    layout='wide'
 )
 
 st.title("Landscape Modification Meets Deep Learning")
--- pages/2_Dynamic_Demo.py
+++ pages/2_Dynamic_Demo.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import base64
-st. set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 st.title('Dynamic Visualization')
 
 
